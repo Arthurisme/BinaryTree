d356c436b2bde4270d765065ada41e79c34371ca
Arthurisme <nebula.trek@gmail.com>
2013-03-11 11:45:31 -0400

    Signed-off-by: Arthurisme <nebula.trek@gmail.com>

--- BinaryTree/src/Testing/Try3.java
+++ BinaryTree/src/Testing/Try3.java
@@ -1,5 +1,8 @@
 package Testing;
 
+import java.util.Date;
+ 
+
 public class Try3 {
 
 	/**
@@ -8,6 +11,13 @@ public class Try3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Date date = new Date();
+		
+		
+		
+		System.out.println(date);
+		
+		//try time.
 	}
 
 }
